240a11556f9b16bd32688493122cac2579a888a7
Erhan Kudretli <erhankudretli@gmail.com>
2020-12-27 21:11:10 +0100

    added linux-commands-packagers-editors.js homework

---
+++ linux-commands-packagers-editors.js
@@ -0,0 +1,28 @@
+$ sudo apt update // tüm repositorylerde güncelleme olup olmadigini günceller
+$sudo apt-get upgrade // Bu komut ile sistemdeki paketler en son sürümlereine güncellenir.
+$ sudo snap install --classic code  // bu komut VS code u sisteme yükler
+$ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb // bu linkde Chrome u indirdi
+$ sudo apt install ./google-chrome-stable_current_amd64.deb // bununla da chromu makinaya kurdu
+$ touch file_name.doc/pptx/xlsx // hocam ben linux makina kullandigim icin bende libreofis yüklü geldi. Ben bahsettiginiz dosyalari da touch komutu ile olustururdum
+$  useradd -aG sudo kullanici_adi // bununla kullanici olusturdum
+$  groupadd grupAdi // bununla grup olusturdum
+$ # usermod -aG grupAdi kullanici_adi // bu satirla kullaniciyi gruba eklemis oldum.
+ssh user@cloud-vm-ip // bununla servere giris yaptik
+vim index.html // ile index isminde html dosyasi actik. i (insert) e basarak icine text yazdik. Islem biitnce de Esc tusuna basarak ciktik
+: , x // tuslarina basarakta yazdiklarimizi kaydettik.
+chown USER[:GROUP] FILE(s) // gosya sahibini degistirme
+$ ls -al   // Dosya ve klasorlerin sahipleri, izinleri ve buyuklukleri gibi detayli bilgilerle, gizli olanlar dahil, listelenmesi icin kullanilir) 
+$ alias //Sizin için hatırlaması yada yazması zor olan bir komutu bir takma ada bağlarsınız buna alias denir.
+ $ df // Salt olarak kullanıldığında sabit disk(ler)in ve bölüm(ler)in (partition) toplam boyutlarını, kullanılmış alanlarını, boş alanlarını, yüzde kaçının dolu olduğunu ve nereye bağlı olduğunu verir. 
+ $ history // Linux’ta çalıştırılan tüm komutlar bir dosyada arşivleniyor ve ihtiyaç duyduğunuzda “history” komutu ile bu komutları görebiliyorsunuz.
+ $ date // mevcut tarih/saati görmek icin kullanilir.
+ $ grep // Diğer bir deyişle, grep komutu ile bir kelime veya kalıbı arayabilir ve buna sahip olan satır veya satırları bastırabilirsiniz
+ $ ps // process'ler hakkkında bilgi verir. 
+ $ kill //  kill komutu durmuş ve duraklamış cevap vermeyen process'ler için kullanılır. 
+ $ tar // Tar komutu (tape archive) dosya gruplarının arşivlenmesinde kullanılır.
+ $ top // Linux islemlerinin gösterilmesi icin kullanilir.
+ $ dz // Linux sistemlerde genellikle disk kullanımını görmek ve hangi uygulamanın yada dosya alt sisteminin ne kadar yer kapladığını görmek için “du” komutunu kullanmak gerekmektedir.
+ $ head // Head komutlarıyla bir dosyanın içeriğinden baştan veye sondan istediğimiz kadar satır görüntüleyebiliyoruz. 
+ $ tail // Head ve tail komutlarıyla bir dosyanın içeriğinden baştan veye sondan istediğimiz kadar satır görüntüleyebiliyoruz.
+ wget // wget, Linux/UNIX komut satırı dosya indirme aracıdır.
+ $ find // Dosylari adlarina ve sahiplerine gore arayip bulma işini "find" komutu ile yapabiliriz.
